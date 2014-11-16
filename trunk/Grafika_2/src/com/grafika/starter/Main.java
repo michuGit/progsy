62905a9f8f5a1c6795c6e8fb5df00b97db3478ee
bledowski.michal@gmail.com <bledowski.michal@gmail.com@285f05e7-0156-a7c9-70f5-8c7fd30fc9f7>
2014-11-16 12:23:22 +0000

    first datas

---
+++ trunk/Grafika_2/src/com/grafika/starter/Main.java
@@ -0,0 +1,17 @@
+package com.grafika.starter;
+
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+	static Logger log = Logger.getLogger(Main.class.getName());
+
+	public static void main(String[] args) throws ClassNotFoundException,
+			InstantiationException, IllegalAccessException,
+			UnsupportedLookAndFeelException {
+		log.info("Grakika_2");
+//		Frame frame = new Frame();
+	}
+
+}
